72f1ba99959a9bb48770b262ee10277f645e75a7
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-01-30 14:54:17 -0500

    quicksave

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"fmt"
 	"net/http"
+	"encoding/json"
 	"sync/atomic"
 )
 
@@ -11,11 +12,34 @@ type apiConfig struct {
 	fileserverHits atomic.Int32
 }
 
+type validateChirpRequest struct {
+	Body string `json:"body"`
+}
+
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w, code, map[string]string{
+		"error": msg,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	data, err := json.Marshal(payload)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(data)
 }
 
 func main() {
@@ -56,6 +80,30 @@ func main() {
 		Handler: mux,
 	}
 
+	mux.HandleFunc("/api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		defer r.Body.Close()
+
+		var req validateChirpRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid JSON")
+			return
+		}
+
+		if len(req.Body) > 140 {
+			respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+			return
+		}
+
+		respondWithJSON(w, http.StatusOK, map[string]bool{
+			"valid": true,
+		})
+	})
+	
 	log.Println("Listening on http://localhost", server.Addr)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
