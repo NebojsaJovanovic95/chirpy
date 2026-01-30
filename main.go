0cd3ed032788a512f948e36f7df6878242f84657
Nebojsa Jovanovic <nebojsajovanovic95@gmail.com>
2026-01-30 10:58:16 -0500

    quicksave

--- main.go
+++ main.go
@@ -7,8 +7,13 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Typ", "text/plain")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"OK"}`))
+	})
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/", fileServer)
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 	server := &http.Server{
 		Addr: ":8080",
 		Handler: mux,
